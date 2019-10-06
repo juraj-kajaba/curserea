1e10d674bacc3d4442984339880cc6a50e835004
juraj-kajaba <juraj.kajaba@gmail.com>
2019-10-06 21:35:42 +0200

    Union find simple and quick union find

--- alg_1/src/alg/quick_union_find.py
+++ alg_1/src/alg/quick_union_find.py
@@ -1,8 +1,9 @@
 
 
 
-class UnionFind:
-    """ Simple union find implementation 
+class QuickUnionFind:
+    """ Quick union find implementation.
+        Trees are balanced by linking root of smaller tree  to the root of larger tree
     """
     def __init__(self, n):
         """ 
@@ -10,7 +11,8 @@ class UnionFind:
         n: number of possible elements
         """
         self.__array = [x for x in range(n)]
-        self.__rootSizes = [1]*n
+        self.__rootSizes = [1]*n        
+        # Depth of tree under root
         self.__rootDepths = [1]*n        
         self.__maxRootSize = 1
         self.__maxRootDepth = 1        
@@ -19,10 +21,8 @@ class UnionFind:
     def getMaxRootSize(self):
         return self.__maxRootSize
 
-
     def getMaxRootDepth(self):
         return self.__maxRootDepth
-
 
     def __getRoot(self, element:int):
         """ Returns root of the input element
@@ -71,8 +71,10 @@ class UnionFind:
         return firstRoot == secondRoot
 
     def doUnion(self, firstEl:int, secondEl:int) -> None:
-        """ Do union of input elements. At first get roots of both elements and
-            afterwards make root of secondEl to be the root of firstEl
+        """ Do union of input elements. At first get roots of both elements then
+            check the size of first and second tree and link the smaller tree under
+            larger tree.
+
 
             Parameters:
             firstEl: first element to be tested if they are connected
@@ -84,13 +86,22 @@ class UnionFind:
         firstRoot = self.__getRoot(firstEl)
         secondRoot = self.__getRoot(secondEl)
 
-        self.__array[firstRoot] = secondRoot
-        self.__rootSizes[secondRoot] += self.__rootSizes[firstRoot]
-        newDepth = self.__rootDepths[secondRoot] + 1
-        self.__rootDepths[secondRoot] = max(self.__rootDepths[secondRoot], newDepth)
-        # self.__rootDepths[secondRoot] += 1
-        self.__maxRootSize = max(self.__maxRootSize, self.__rootSizes[secondRoot])
-        self.__maxRootDepth = max(self.__maxRootDepth, self.__rootDepths[secondRoot])
+        if self.__rootSizes[secondRoot] <= self.__rootSizes[firstRoot]:
+            self.__array[secondRoot] = firstRoot
+            self.__rootSizes[firstRoot] += self.__rootSizes[secondRoot]
+            newDepth = self.__rootDepths[secondRoot] + 1 # Get new depth of tree after addition under firstRoot
+            self.__rootDepths[firstRoot] = max(self.__rootDepths[firstRoot], newDepth)
+            # self.__rootDepths[firstRoot] += 1
+            self.__maxRootSize = max(self.__maxRootSize, self.__rootSizes[firstRoot])
+            self.__maxRootDepth = max(self.__maxRootDepth, self.__rootDepths[firstRoot])            
+        else:
+            self.__array[firstRoot] = secondRoot
+            self.__rootSizes[secondRoot] += self.__rootSizes[firstRoot]
+            newDepth = self.__rootDepths[firstRoot] + 1 # Get new depth of tree after addition under secondRoot
+            self.__rootDepths[secondRoot] = max(self.__rootDepths[secondRoot], newDepth)
+            # self.__rootDepths[secondRoot] += 1
+            self.__maxRootSize = max(self.__maxRootSize, self.__rootSizes[secondRoot])
+            self.__maxRootDepth = max(self.__maxRootDepth, self.__rootDepths[secondRoot])            
 
 
     def getArray(self):
--- alg_1/tests/test_union_find.py
+++ alg_1/tests/test_union_find.py
@@ -1,16 +1,57 @@
 from alg import union_find as uf
+from alg import quick_union_find as quf
 
-ufo = uf.UnionFind(20)
+def test_uf_1():
+    ufo = uf.UnionFind(20)
 
-connections = [(1, 2), (1,3), (1,4), (1,5), (2,6)]
+    connections = [(1, 2), (1,3), (1,4), (1,5), (2,6)]
 
-for i in connections:
-    ufo.doUnion(i[0], i[1])
+    for i in connections:
+        ufo.doUnion(i[0], i[1])
 
-for i in connections:
-    assert ufo.isConnected(i[0], i[1]) == True
+    for i in connections:
+        assert ufo.isConnected(i[0], i[1]) == True
+
+    print(ufo.getArray())
+    print(ufo.getRootSizes())
+    print(ufo.getMaxRootSize())
 
 
+def test_uf_2():
+    ufo = uf.UnionFind(10)
 
-print(ufo.getArray())
-print(ufo.getRootSizes())
+    connections = [(4, 3), (3, 8), (6, 5), (9, 4), (2, 1), (5, 0), (7, 2), (6,1), (7, 3)]
+
+    for i in connections:
+        ufo.doUnion(i[0], i[1])
+
+    for i in connections:
+        assert ufo.isConnected(i[0], i[1]) == True
+
+    print(ufo.getArray())
+    print(ufo.getRootSizes())
+    print(ufo.getRootDepths())    
+    print(ufo.getMaxRootSize())
+    print(ufo.getMaxRootDepth())
+
+
+def test_quf():
+    ufo = quf.QuickUnionFind(10)
+
+    connections = [(4, 3), (3, 8), (6, 5), (9, 4), (2, 1), (5, 0), (7, 2), (6,1), (7, 3)
+    ]
+
+    for i in connections:
+        ufo.doUnion(i[0], i[1])
+
+    for i in connections:
+        assert ufo.isConnected(i[0], i[1]) == True
+
+    print(ufo.getArray())
+    print(ufo.getRootSizes())
+    print(ufo.getRootDepths())    
+    print(ufo.getMaxRootSize())
+    print(ufo.getMaxRootDepth())
+
+test_uf_2()
+test_quf()
